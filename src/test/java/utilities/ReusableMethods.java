35c37777c82b1984aa1d4abb9d8e29f5f89dd1d9
bulutluoz <abbulutluoz@gmail.com>
2024-11-27 11:23:29 +0100

    day04 sonu

---
+++ src/test/java/utilities/ReusableMethods.java
@@ -0,0 +1,17 @@
+package utilities;
+
+public class ReusableMethods {
+
+    public static void bekle(int saniye ){
+
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            System.out.println("Thread.sleep calismadi");
+        }
+
+
+    }
+
+
+}
