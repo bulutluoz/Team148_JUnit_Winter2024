de87b2b51c41c6db408361c4c63b3cc429279987
bulutluoz <abbulutluoz@gmail.com>
2024-11-27 11:49:41 +0100

    day04 sonu

---
+++ src/test/java/day04_mavenProjeKullanimi/C03_WebAppLoginTesti.java
@@ -0,0 +1,20 @@
+package day04_mavenProjeKullanimi;
+
+public class C03_WebAppLoginTesti {
+
+    public static void main(String[] args) {
+        //1. http://zero.webappsecurity.com
+        //    sayfasina gidin
+        //2. Signin buttonuna tiklayin
+        //3. Login alanine  “username” yazdirin
+        //4. Password alanina “password” yazdirin
+        //5. Sign in buttonuna tiklayin
+        //6. Back tusu ile sayfaya donun
+        //7. Online Banking menusunden Pay Bills sayfasina gidin
+        //8. amount kismina yatirmak istediginiz herhangi bir miktari yazin
+        //9. tarih kismina “2023-09-10” yazdirin
+        //10. Pay buttonuna tiklayin
+        //11. “The payment was successfully submitted.” mesajinin ciktigini test edin
+
+    }
+}
