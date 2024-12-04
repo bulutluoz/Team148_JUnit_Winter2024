997983eeb8d999e0917bad46b6b6904ea342b83c
bulutluoz <abbulutluoz@gmail.com>
2024-12-04 09:50:32 +0100

    Ders08 sonu

--- src/test/java/day08_windowSwitch_actionsClass/C01_KontrolluWindowDegistirme.java
+++ src/test/java/day08_windowSwitch_actionsClass/C01_KontrolluWindowDegistirme.java
@@ -11,6 +11,25 @@ public class C01_KontrolluWindowDegistirme extends TestBaseEach {
     @Test
     public void test01(){
 
+        /*
+
+            Eger bize verilen gorevde
+            yeni bir Tab veya yeni bir Window acilmasi isteniyorsa
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.switchTo().newWindow(WindowType.WINDOW);
+            ile yeni bir Tab veya Window acip, driver'i yeni window'a otomatik olarak gecirebiliriz
+
+            Eger testimiz sirasinda birden fazla window aciliyorsa
+            driver'i istedigimiz window'a gecirmek icin
+            hedef window'un WindowHandle degerini girmeliyiz
+            driver.switchTo().window(wisequarterWhd);
+
+            Eger birden fazla window aciyorsak ve geri donmemiz gerekecekse
+            acilan her window'un window handle degerini kaydetmek
+            faydali olacaktir.
+
+         */
+
         // testotomasyonu sayfasina gidin
         driver.get("https://www.testotomasyonu.com");
 
@@ -40,11 +59,6 @@ public class C01_KontrolluWindowDegistirme extends TestBaseEach {
         // wisequarter'in acik oldugu window'a donun
         // ve url'in wisequarter icerdigini test edin
 
-        /*
-            Eger testimiz sirasinda birden fazla window aciliyorsa
-            driver'i istedigimiz window'a gecirmek icin
-            hedef window'un WindowHandle degerini girmeliyiz
-         */
 
         driver.switchTo().window(wisequarterWhd);
         String expectedUrlIcerik = "wisequarter";
