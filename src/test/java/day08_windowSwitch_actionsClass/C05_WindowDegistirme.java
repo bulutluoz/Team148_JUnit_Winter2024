0209b310be4b4abd0142949a3e532432d107a135
bulutluoz <abbulutluoz@gmail.com>
2024-12-04 11:09:23 +0100

    Ders08 sonu

--- src/test/java/day08_windowSwitch_actionsClass/C05_WindowDegistirme.java
+++ src/test/java/day08_windowSwitch_actionsClass/C05_WindowDegistirme.java
@@ -3,6 +3,7 @@ package day08_windowSwitch_actionsClass;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.ReusableMethods;
 import utilities.TestBaseEach;
 
@@ -28,19 +29,36 @@ public class C05_WindowDegistirme extends TestBaseEach {
 
 
         // ilk window'a geri donun
-
+        ReusableMethods.titleIleWindowDegistir(driver,"The Internet");
 
         // "Opening a new window" yazisinin gorunur oldugunu test edin
+        WebElement yaziElementi = driver.findElement(By.tagName("h3"));
 
+        Assertions.assertTrue(yaziElementi.isDisplayed());
 
         // Click here'a basin
-
+        driver.findElement(By.xpath("//*[.='Click Here']"))
+                .click();
 
         // Acilan yeni window'a gecip, oradaki yazinin "New Window" oldugunu test edin
+        ReusableMethods.titleIleWindowDegistir(driver,"New Window");
+
+        yaziElementi = driver.findElement(By.tagName("h3"));
+
+        expectedYazi = "New Window";
+        actualYazi = yaziElementi.getText();
+
+        Assertions.assertEquals(expectedYazi,actualYazi);
 
 
         // Elemental selenium'un acik oldugu window'a gecip
         // Tips linkinin erisilebilir oldugunu test edin
+
+        ReusableMethods.urlIleWindowDegistir(driver,"https://elementalselenium.com/");
+
+        WebElement tipsElementi = driver.findElement(By.xpath("//a[.='Tips']"));
+
+        Assertions.assertTrue(tipsElementi.isEnabled());
 
         ReusableMethods.bekle(2);
 
