8172d792752cf432bf16faf6bf7fc08d498ff542
bulutluoz <abbulutluoz@gmail.com>
2024-12-09 10:47:34 +0100

    Konu10 sonu

---
+++ src/test/java/day10_waits_cookies/C03_OlmayanElementiTestEtme.java
@@ -0,0 +1,19 @@
+package day10_waits_cookies;
+
+import org.junit.jupiter.api.Test;
+import utilities.TestBaseEach;
+
+public class C03_OlmayanElementiTestEtme extends TestBaseEach {
+
+    @Test
+    public void test01(){
+
+        //1. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        //2. Remove butonuna basin.
+        //3. checkbox'in gorunur olmadigini test edin
+        //4. Add buttonuna basin
+        //5. checkbox'in gorunur oldugunu test edin
+
+    }
+
+}
