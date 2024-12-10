58e0065c0d442d7ee4d18094a382a8816a9c3229
bulutluoz <abbulutluoz@gmail.com>
2024-12-10 12:12:37 +0100

    Ders11 sonu

--- src/test/java/day11_WebTables_ExcelOtomasyon/C03_ReadExcel.java
+++ src/test/java/day11_WebTables_ExcelOtomasyon/C03_ReadExcel.java
@@ -1,8 +1,6 @@
 package day11_WebTables_ExcelOtomasyon;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
@@ -35,9 +33,36 @@ public class C03_ReadExcel {
         // 4.adim excel'in kopyasi olan workbook'da
         //        istedigimiz bilgiye ulasalim
 
+        // Sayfa1'deki 5.satir, 3.hucrede olan bilgiyi yazdirin
+        // workbook da Java'daki genel kabule uygun olarak
+        // index kullanir
+        // index 0'dan basladaigi icin
+        // 5.satir icin index==>4, 3.hucre icin index==>2 secilmelidir
+
         Sheet sheet1 = workbook.getSheet("Sayfa1");
 
+        Row row = sheet1.getRow(4);
 
+        Cell cell = row.getCell(2);
+
+        System.out.println("Sayfa1 5.satir, 3.sutun : "+ cell); // Andorra
+
+
+        // her seferinde row ve cell olusturmaya gerek yok
+        // Sayfa1'deki 15.satir, 2.hucrede olan bilgiyi yazdirin
+
+        System.out.println(workbook.getSheet("Sayfa1").getRow(14).getCell(1)); // Dhaka
+
+
+        // son satir numarasini yazdirin
+        System.out.println(workbook.getSheet("Sayfa1").getLastRowNum()); // 190
+        // bu son satirin index'idir
+
+        System.out.println("Son satir no : " + (workbook.getSheet("Sayfa1").getLastRowNum()+1));
+
+
+        // Kullanilan satir sayisini yazdirin
+        System.out.println(workbook.getSheet("Sayfa1").getPhysicalNumberOfRows()); // 191
 
 
     }
---
+++ src/test/java/day11_WebTables_ExcelOtomasyon/C04_ReadExcel.java
@@ -0,0 +1,12 @@
+package day11_WebTables_ExcelOtomasyon;
+
+import org.junit.jupiter.api.Test;
+
+public class C04_ReadExcel {
+
+    @Test
+    public void test01(){
+
+
+    }
+}
