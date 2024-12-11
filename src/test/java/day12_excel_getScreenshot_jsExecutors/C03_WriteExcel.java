6d9eb893bc65a0eb0e33bfe28ea4265d6cb43725
bulutluoz <abbulutluoz@gmail.com>
2024-12-11 10:25:16 +0100

    Ders12 sonu

--- src/test/java/day12_excel_getScreenshot_jsExecutors/C03_WriteExcel.java
+++ src/test/java/day12_excel_getScreenshot_jsExecutors/C03_WriteExcel.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class C03_WriteExcel {
@@ -27,10 +28,51 @@ public class C03_WriteExcel {
         sayfa1.getRow(0).getCell(4).setCellValue("Nufus");
 
         //6) 2.satir nufus kolonuna 1500000 yazdiralim
+        sayfa1.getRow(1).createCell(4).setCellValue(1500000);
+
         //7) 10.satir nufus kolonuna 250000 yazdiralim
+        sayfa1.getRow(9).createCell(4).setCellValue(250000);
+
         //8) 15.satir nufus kolonuna 54000 yazdiralim
+        sayfa1.getRow(14).createCell(4).setCellValue(54000);
+
+
+        // bos olan ilk satira
+        // ingilizce ulke ismi JavaRepublic  ingilizce baskent olarak Selenium yazdiralim
+
+        int ilkBosSatirIndexi = sayfa1.getLastRowNum()+1;
+
+        sayfa1.createRow(ilkBosSatirIndexi);
+        sayfa1.getRow(ilkBosSatirIndexi).createCell(0).setCellValue("JavaRepublic");
+        sayfa1.getRow(ilkBosSatirIndexi).createCell(1).setCellValue("Selenium");
+
+
         //9) Dosyayi kaydedelim
+        /*
+            Biz yaptigimiz tum degisikleri
+            workbook uzerinde yaptik
+
+            workbook objesi fiziki excel dosyasinda
+            FileInputStream ile aldigimiz bilgilerle olusturuldu
+            ve excel'in bir kopyasi oldu
+
+            eger workbook uzerinde yaptigimiz degisiklikleri
+            excel dosyasina islemek istiyorsak
+            kaydetme islemi yapmamiz gerekir.
+
+            ONEMLI NOT :
+            workbook'da yaptigimiz degisiklikleri
+            excel'e kaydetme islemi yapmadan once
+            excel dosyasinin kapali oldugundan emin OLMALISINIZ.
+         */
+        FileOutputStream fileOutputStream = new FileOutputStream(dosyaYolu);
+        workbook.write(fileOutputStream);
+
         //10)Dosyayi kapatalim
+        fileInputStream.close();
+        fileOutputStream.close();
+        workbook.close();
+
     }
 
 
